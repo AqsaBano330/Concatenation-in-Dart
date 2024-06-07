60cb566105b3d1ccb928aed272e0955c3b34e5c8
AqsaBano330 <aqsabano08@gmail.com>
2024-06-07 20:50:42 +0500

    Concatenation in dart

---
+++ concatenation.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  String firstname = "Aqsa";
+  String lastname = "Bano";
+  int age = 12;
+  String city = "karachi";
+
+  print(" My name is $firstname $lastname, my age is $age and my city is $city"); 
+
+  
+}
